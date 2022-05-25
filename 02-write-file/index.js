ffefad98b893d7f0028c56615dce31a92833f0eb
vMUSTANGv <80420955+vMUSTANGv@users.noreply.github.com>
2022-05-25 12:55:48 +0300

    Feat: 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,13 +1,23 @@
 const { stdin, stdout, exit } = process;
 const fs = require('fs');
 const path = require('path');
+const file = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
-stdout.write('Добрый день! Введите строку\n');
-stdin.on('data', data => {
-  fs.writeFile(path.join(__dirname, 'text.txt'), data, err => {
-    if (err) {
-      throw err;
-    }
-    exit();
-  });
+stdout.write('Доброго дня! Введите строку!..\n');
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit'){
+    bye();
+  }
+  file.write(data);
 });
+
+process.on('SIGINT', function(){
+  process.stdout.write('\nВсего доброго');
+  process.exit();
+});
+
+
+function bye(){
+  stdout.write('Всего доброго!\n');
+  exit();
+}
