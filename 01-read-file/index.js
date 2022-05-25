f4e1681a0e4d0950b29509e0e3802231d15e848e
vMUSTANGv <80420955+vMUSTANGv@users.noreply.github.com>
2022-05-25 12:02:34 +0300

    Feat: 01-reade-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
+const stream = fs.createReadStream(path.join(__dirname, 'text.txt'));
+stream.on('data', function (item) {
+  console.log(item.toString());
+});
+// console.log(stream);
